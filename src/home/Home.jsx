2a1decd12e5b1f17fc180ad39204f5ee2aafd4c1
AlinaGimp <alinagimp.v@gmail.com>
2022-10-25 20:19:24 +0300

    Created page allows to switch between projects using React Router

---
+++ src/home/Home.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import './HomeStyles.css';
+import {
+  Link,
+  Outlet,
+} from 'react-router-dom';
+
+function Home() {
+  return (
+    <div className="projects">
+      <h1>My projects</h1>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/chat">ChatPage</Link>
+            </li>
+            <li>
+              <Link to="/weather">ChartWeather</Link>
+            </li>
+            <li>
+              <Link to="/to-do-list">ToDoList</Link>
+            </li>
+            <li>
+              <Link to="/user">UserPage</Link>
+            </li>
+          </ul>
+        </nav>
+        <Outlet />
+      </div>
+    </div>
+  );
+}
+
+export default Home;
