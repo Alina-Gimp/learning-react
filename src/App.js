adc5ee5ce96824e3e03417d438c4c798925dfcf4
AlinaGimp <alinagimp.v@gmail.com>
2022-10-25 20:25:14 +0300

    React routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+import React from 'react';
+import {
+  Routes,
+  Route,
+} from 'react-router-dom';
+import Home from './home/Home';
+import ChatPage from './chat/ChatPage';
+import ChartWeather from './weather/ChartWeather';
 import ToDoList from './to-do-list/ToDoList';
+import UserPage from './crm/user/UserPage';
 
-function App() {
+export default function App() {
   return (
-    <ToDoList />
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="chat" element={<ChatPage />} />
+        <Route path="weather" element={<ChartWeather />} />
+        <Route path="to-do-list" element={<ToDoList />} />
+        <Route path="user" element={<UserPage />} />
+      </Routes>
+    </div>
   );
 }
-
-export default App;
